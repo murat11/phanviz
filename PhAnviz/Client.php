57fac4bb5bc58fc8b182911448d5abe36a398451
Murat Erkenov <murat@11bits.net>
2020-01-02 23:57:28 +0300

    Client layer added

---
+++ PhAnviz/Client.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace PhAnviz;
+
+use Generator;
+use PhAnviz\Transport\TransportInterface;
+
+class Client
+{
+    /**
+     * @var string
+     */
+    private $deviceId;
+
+    /**
+     * @var TransportInterface
+     */
+    private $transport;
+
+    /**
+     * Client constructor.
+     *
+     * @param string $deviceId
+     * @param TransportInterface $transport
+     */
+    public function __construct(string $deviceId, TransportInterface $transport)
+    {
+        $this->deviceId = $deviceId;
+        $this->transport = $transport;
+    }
+
+    /**
+     * @param int    $command
+     * @param string $data
+     *
+     * @return \Generator
+     */
+    public function request(int $command, string $data = null)
+    {
+        $req = $this->createRequestString($command, $data);
+        $response = $this->transport->req($req);
+        foreach (explode(PHP_EOL, $response) as $responseItem) {
+            yield self::parseResponse($responseItem);
+        }
+    }
+
+    /**
+     * @param array $commands
+     *
+     * @return Generator
+     */
+    public function requestMulti(array $commands)
+    {
+        foreach ($commands as list($command, $data)) {
+            yield $this->request($command, $data);
+        }
+    }
+
+    /**
+     * @param int    $command
+     * @param string $data
+     *
+     * @return bool|string
+     */
+    private function createRequestString(int $command, string $data = null)
+    {
+        $req = sprintf(
+            "a5%08s%02x%04x%s",
+            $this->deviceId,
+            $command,
+            !empty($data) ? strlen($data) / 2 : 0,
+            $data
+        );
+        $req .= CRC16::crc16(hex2bin($req));
+        $req = hex2bin($req);
+
+        return $req;
+    }
+
+    /**
+     * Convert response from:
+     *
+     * STX      CH(device code)     ACK(response)               RET(return)     LEN(data length)    DATA            CRC16
+     * 0xA5     4 Bytes             1 Byte(command + 0x80)      1 Byte          2 Bytes             0-400 Bytes     2 Bytes
+     *
+     * to array
+     *
+     * @param string $response
+     *
+     * @return array
+     */
+    private static function parseResponse($response)
+    {
+        $response = implode(unpack("H*", $response));
+
+        $resArr = str_split($response, 2);
+
+        $result = [
+            'stx' => implode(array_slice($resArr, 0, 1)),
+            'ch'  => hexdec(implode(array_slice($resArr, 1, 4))),
+            'ack' => hexdec(implode(array_slice($resArr, 5, 1))),
+            'ret' => implode(array_slice($resArr, 6, 1)),
+            'len' => hexdec(implode(array_slice($resArr, 7, 2))),
+            'crc' => implode(array_slice($resArr, -2, 2)),
+        ];
+        $result['data'] = array_slice($resArr, 9, $result['len']);
+
+        return $result;
+    }
+}
---
+++ Test/ClientTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use PhAnviz\Client;
+use PhAnviz\Transport\TransportInterface;
+use PHPUnit\Framework\TestCase;
+
+class ClientTest extends TestCase
+{
+    public function testRequestOk()
+    {
+        $transport = $this->createMock(TransportInterface::class);
+        $pieces = [
+            hex2bin('a500000001bc000012000025000029000002000000004dce00000071f6'),
+            hex2bin('a500000001df00000e00000000122593831902020000009300')
+        ];
+        $transport->method('req')->willReturn(implode(PHP_EOL, $pieces));
+        $client = new Client('1', $transport);
+        $response = $client->request(0x12, sprintf('%02x%02x', 0, 12));
+        $this->assertIsIterable($response);
+
+        foreach ($response as $responseItem) {
+            $this->assertIsArray($responseItem);
+        }
+    }
+
+    public function testClientMulti()
+    {
+        $client = $this->createPartialMock(Client::class, ['request']);
+        $client->expects($this->exactly(2))->method('request')->withConsecutive([0x01, '0x0101'], [0x02, '0x0202'])->willReturn('aaa');
+
+        $multiResponse = $client->requestMulti([[0x01, '0x0101'], [0x02, '0x0202']]);
+        foreach ($multiResponse as $responseItem) {
+            $this->assertEquals('aaa', $responseItem);
+        }
+    }
+}
